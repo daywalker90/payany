23509d975e22607f94fd6c9537d09e464e5d3c7f
daywalker90 <8257956+daywalker90@users.noreply.github.com>
2026-04-23 11:49:01 +0200

    tests: try to fix race condition in route hint creation flake

--- tests/test_payany.py
+++ tests/test_payany.py
@@ -418,6 +418,14 @@ def test_pay_to_xpay_fees(node_factory, get_plugin):  # noqa: F811
             == 10000
         )
     )
+    wait_for(
+        lambda: (
+            l3.rpc.call("listchannels", {"destination": l3.info["id"]})["channels"][0][
+                "base_fee_millisatoshi"
+            ]
+            == 10000
+        )
+    )
 
     invoice = l3.rpc.call("invoice", [950000, "test", "test"])
 
